546256a9414acd5e8b184d6f1b019120ca48514f
Rob Dodson <robdodson@google.com>
2014-10-24 19:07:45 -0700

    Add page structure and fab

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,11 +1,15 @@
 (function(document) {
   'use strict';
 
-  document.addEventListener('polymer-ready', function() {
-    // Perform some behaviour
-    console.log('Polymer is ready to rock!');
+  var tmpl = document.querySelector('template');
+  tmpl.selected = 0;
+  tmpl.addEventListener('template-bound', function() {
+    var pages = document.querySelector('core-animated-pages');
+    this.showPeople = function() {
+      pages.selected = 1;
+    }
   });
 
-// wrap document so it plays nice with other libraries
-// http://www.polymer-project.org/platform/shadow-dom.html#wrappers
+  // wrap document so it plays nice with other libraries
+  // http://www.polymer-project.org/platform/shadow-dom.html#wrappers
 })(wrap(document));
